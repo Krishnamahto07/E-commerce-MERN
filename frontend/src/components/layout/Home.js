c957b88901e18125927f6617efc40780f178a370
Krishna Mahto <krishnamahato84044@gmail.com>
2024-04-08 23:25:18 +0530

    fifth-commit

--- frontend/src/components/layout/Home.js
+++ frontend/src/components/layout/Home.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import { CgMouse } from "react-icons/cg";
 
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { Product } from './Product';
 export const Home = () => {
+  const product = {
+    name:"Blue Tshirt",
+    images:[{url:"https://static.cilory.com/480395-thickbox_default/octane-blue-full-sleeves-t-shirt-by-nologo.jpg"}],
+    price:"Rs. 3000",
+    _id:"abhishek",
+  };
   return (
-    <>
-      <div className='w-full bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-full
-      flex flex-col justify-center items-center gap-9 py-8 text-white'>
+    <div className='w-screen bg-[#124076]'>
+      <div className='flex flex-col justify-center items-center gap-9 py-8 text-white'>
         <p className='text-3xl font-semibold'>Welcome to E commerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-        <Link to="/container"> 
-        <button type="button" class="flex gap-2 justify-center items-center bg-gradient-to-r from-green-300 to-gray-900  hover:from-pink-500 hover:to-yellow-500 
-      transition-all duration-500 ease-in hover:scale-90 px-4 py-2 rounded-lg ">
-          <spna>Scroll</spna> <CgMouse/>
-        </button>
-        </Link>
+        <a href="#container"> 
+          <button type="button" className="flex gap-2 justify-center items-center bg-gradient-to-r from-green-100 to-gray-200  hover:from-pink-500 hover:to-yellow-500 
+            transition-all duration-500 ease-in hover:scale-90 px-8 py-4 rounded-lg text-gray-600 hover:text-white">
+            <span>Scroll</span> <span className='text-2xl'><CgMouse/></span>
+          </button>
+        </a>
       </div>
-    </>
+
+      <h2 className='shadow-2xl text-center
+      text-2xl font-semibold text-white underline uppercase '>Featured Products</h2>
+      <div className='w-full h-[400px]'>
+        hlw
+      </div>
+      <div id='container' className='flex w-full flex-wrap mx-auto justify-center items-center'>
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+      </div>
+    </div>
   )
 }
--- frontend/src/components/layout/Product.js
+++ frontend/src/components/layout/Product.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ReactStars from "react-rating-stars-component";
 
-export const Product = () => {
+export const Product = ({product}) => {
+  const options = {
+    edit:false,
+    color:"rgba(20,20,20,0.1)",
+    activeColor:"tomato",
+    size:window.innerWidth < 600 ? 20 : 25,
+    value:2.5,
+    isHalf:true,
+  };
   return (
-    <div className='w-full h-full bg-green-950 text-5xl'> I am Product</div>
+    
+    <Link to={product._id}
+    className='w-[18vmax] flex flex-col 
+    text-[rgb(48,48,48)] m-[2vmax]  pb-[0.5vmax] border border-black p-2 rounded-md hover:scale-105 transition-all duration-300 ease-out
+    bg-blue-200'>
+      <img src={product.images[0].url} alt={product.name} 
+      className='w-[18vmax] rounded-sm'/>
+      <p className='text-xl font-bold mt-1 text-gray-800'>{product.name}</p>
+      <div className='flex flex-col'>
+        <div className=''><ReactStars {...options} /></div>
+        <span className='text-gray-700'>(256 Reviews)</span>
+      </div>
+      <span className='text-xl font-bold text-red-500'>{product.price}</span>
+    </Link>
   )
 }
